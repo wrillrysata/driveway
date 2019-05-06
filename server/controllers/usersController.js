521ce051e3ff2f596b241b160b722f327cfac179
Ijebusoma <vanessaosuka@gmail.com>
2019-05-06 13:30:37 +0100

    feature(user-signup-api): Write code for user validaton and fix linting

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,26 +1,31 @@
 import db from '../models/index';
+
 /**
  *@class usersController
  *
  * @export
  *
  */
-export default class usersController{
-/**
+export default class usersController {
+  /**
    * @description - Creates a new user
    * @static
    *
-   * @param {object} req - HTTP Request
-   * @param {object} res - HTTP Response
+   * @param {Object} req - HTTP Request.
+   * @param {Object} res - HTTP Response.
    *
    * @memberof usersController
    *
-   * @returns {object} Class instance
+   * @returns {Object} Class instance.
    */
   static createUser(req, res) {
-    const { username, email, password } = req;
-    res.send({
-        username, email
-    })
+    const {
+      username,
+      email,
+      bio,
+      location,
+      password,
+      confirmPassword,
+    } = req.body;
   }
 }
--- server/middleware/userValidation.js
+++ server/middleware/userValidation.js
@@ -1,3 +1,32 @@
-export const verifyUserSignUp =(req, res, next) => {
-const { username, email, password, bio, location } = req;
-}
+import { isEmpty } from 'lodash';
+import validator from 'validator';
+
+export const verifyUserSignUp = (req, res, next) => {
+  const { username, email, location, password, confirmPassword } = req.body;
+  const errors = {};
+  if (!username) {
+    errors.username = 'Username is required';
+  } else if (username && validator.isEmpty(username.trim())) {
+    errors.username = 'Username cannot be empty';
+  } else if (!email) {
+    errors.email = 'Email is required';
+  } else if (email && !validator.isEmail(email.trim())) {
+    errors.email = 'Email is invalid or empty';
+  } else if (!location) {
+    errors.location = 'Location is required';
+  } else if (!password) {
+    errors.password = 'Password is required';
+  } else if (!confirmPassword) {
+    errors.confirmPassword = 'Please confirm your password';
+  } else if (
+    validator.isEmpty(password) ||
+    validator.isEmpty(confirmPassword) ||
+    confirmPassword.trim() !== password.trim()
+  ) {
+    errors.confirmPassword = "Passwords don't match";
+  }
+  if (isEmpty(errors)) {
+    return next();
+  }
+  return res.status(400).json({ errors });
+};
