36f8bc2baf66b9d4859af34370e152af69c18e8c
Ijebusoma <vanessaosuka@gmail.com>
2019-05-06 13:28:28 +0100

    Add missing body-parser module

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import routes from './routes';
 
 dotenv.config();
@@ -9,14 +10,17 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 routes(app);
 app.get('/', (req, res) => {
-    res.status(200).send('Welcome to Driveway app');
+  res.status(200).send('Welcome to Driveway app');
 });
 
 app.use('*', (req, res) =>
-    res.send({
-        message: 'The API route you requested does not exist'
-    }));
+  res.send({
+    message: 'The API route you requested does not exist',
+  })
+);
 app.listen(port, () => console.log(`Driveway app listening on port ${port}`));
 export default app;
