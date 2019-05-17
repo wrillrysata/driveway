7bd3f87192e90b1ad3699903a53f4e065cb8d70e
Vanessa Osuka <vanessaosuka@gmail.com>
2019-05-17 21:59:26 +0100

    feature(users-crud): Implements remaining part of users profile" (#10)
    This commit implements necessary feature for users
    profile that is the RUD part.
    
    [Ticket: #s166009136]

---
+++ server/middleware/authorization.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+const authorization = (req, res, next) => {
+  const token = req.headers.token || req.body.token || req.query.token;
+
+  try {
+    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
+    req.params.userId = verifyToken.id;
+    req.username = verifyToken.username;
+    return next();
+  } catch (error) {
+    return res.status(401).send({
+      errors: {
+        status: '401',
+        title: 'Unauthorized',
+        detail: 'You are not authorized to perform this action',
+      },
+    });
+  }
+};
+
+export default authorization;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,8 +3,12 @@ import {
   verifyUserSignUp,
   verifyUserSignin,
 } from '../middleware/userValidation';
+import { verifyUserId } from '../middleware/idValidation';
+import authorization from '../middleware/authorization';
 
 export default function userRoutes(app) {
   app.post('/api/v1/users/signup', verifyUserSignUp, user.createUser);
   app.post('/api/v1/users/signin', verifyUserSignin, user.userLogin);
+  app.get('/api/v1/users/:userId/profile', verifyUserId, user.userProfile);
+  app.put('/api/v1/users/:userId/profile', authorization, user.editUserProfile);
 }
